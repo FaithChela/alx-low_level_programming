40ecd16e1a1c75646ca59f958e16d33d40e14380
FaithChela <faitharuonga@gmail.com>
2023-07-07 06:10:00 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, f, a;
+
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		a = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				a = 1;
+			}
+		}
+		if (a == 0)
+		{
+			return (f);
+		}
+	}
+
+	return (0);
+}
